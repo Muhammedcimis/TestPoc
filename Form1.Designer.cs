20f32fb86c876f936b153127d366ed9dee5d1af5
Muhammedcimis <Muhammedcimis@Z29-15>
2022-02-18 18:49:08 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace TestPoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.text_kayit = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_kayit = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(9, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Excel Dosyası Seç";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 96);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(831, 372);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(664, 21);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "TEXT olarak aktar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // text_kayit
+            // 
+            this.text_kayit.Location = new System.Drawing.Point(440, 37);
+            this.text_kayit.Margin = new System.Windows.Forms.Padding(2);
+            this.text_kayit.Name = "text_kayit";
+            this.text_kayit.Size = new System.Drawing.Size(115, 20);
+            this.text_kayit.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkOrange;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(913, 141);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 49);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Yıl sayısını getir";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(913, 93);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(913, 117);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(143, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(844, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Aranacak yıl";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(863, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Yıl sayısı";
+            // 
+            // label_kayit
+            // 
+            this.label_kayit.AutoSize = true;
+            this.label_kayit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_kayit.Location = new System.Drawing.Point(216, 38);
+            this.label_kayit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_kayit.Name = "label_kayit";
+            this.label_kayit.Size = new System.Drawing.Size(220, 16);
+            this.label_kayit.TabIndex = 17;
+            this.label_kayit.Text = "Kaydedilen toplam satır sayısı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 478);
+            this.Controls.Add(this.label_kayit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.text_kayit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "TestPOC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox text_kayit;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_kayit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace TestPoc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string tamYol;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası |*.xlsx";
+            file.ShowDialog();
+            tamYol = file.FileName;
+            
+            string baglantiAdresi = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + tamYol + ";Extended Properties='Excel 12.0;IMEX=1;'";
+            OleDbConnection baglanti = new OleDbConnection(baglantiAdresi);
+            OleDbCommand komut = new OleDbCommand("Select * From [" + "welcome" + "$]", baglanti);
+            baglanti.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+            DataTable data = new DataTable();
+            da.Fill(data);
+            dataGridView1.DataSource = data;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string baglantiAdresi = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + tamYol + ";Extended Properties='Excel 12.0;IMEX=1;'";
+            OleDbConnection baglanti = new OleDbConnection(baglantiAdresi);
+
+            OleDbCommand komut = new OleDbCommand("Select * From [" + "welcome" + "$] Where Tarih like '%" + textBox2.Text.ToString() + "' ", baglanti);
+
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+
+            DataTable data = new DataTable();
+            da.Fill(data);
+            int sayac = 0;
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                sayac++;
+            }
+            textBox3.Text = sayac.ToString();
+
+
+            //-----------------------------------------------------------------------------------------------------------------------
+            //-------------- 2.yol. Bu yol datagrid üzerinden çalışmaktadır.Sadece exceldeki yıllara göre arama yapmaktadır.---------
+            //-----------------------------------------------------------------------------------------------------------------------
+
+            //DateTime date = new DateTime(2021, 01, 1);
+            //DateTime date2 = new DateTime(2020, 01, 1);
+            //DateTime date3 = new DateTime(2019, 01, 1);
+            //DateTime date4 = new DateTime(2018, 01, 1);
+            //DateTime date5 = new DateTime(2017, 01, 1);
+
+            //int sayac2021 = 0, sayac2020 = 0, sayac2019 = 0, sayac2018 = 0, sayac2017 = 0;
+            //string s = "Tarih";
+
+            //for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            //{
+            //    if (dataGridView1.Columns[i].HeaderText == s)
+            //    {
+            //        for (int j = 0; j < dataGridView1.Rows.Count - 1; j++)
+            //        {
+            //            foreach (DataGridViewCell cell in dataGridView1.Rows[j].Cells)
+            //            {
+            //                if (cell.Value != null)
+            //                {
+            //                    string yil = cell.Value.ToString();
+            //                    DateTime.TryParse(yil, out DateTime sonyil);
+
+            //                    //DateTime d2 = Convert.ToDateTime(cell.Value);
+
+            //                    int durum = DateTime.Compare(date,sonyil);
+
+            //                    if (sonyil.Year == date.Year)
+            //                    {
+            //                        sayac2021++;                                    
+            //                    }
+            //                    else if (sonyil.Year == date2.Year)
+            //                    {
+            //                        sayac2020++;                                   
+            //                    }
+            //                    else if (sonyil.Year == date3.Year)
+            //                    {
+            //                        sayac2019++;                                   
+            //                    }
+            //                    else if (sonyil.Year == date4.Year)
+            //                    {
+            //                        sayac2018++;                                    
+            //                    }
+            //                    else if (sonyil.Year == date5.Year)
+            //                    {
+            //                        sayac2017++;                                   
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            //string arananyil=textBox2.Text.ToString();
+
+            //if (arananyil=="2021")
+            //{
+            //    textBox3.Text = sayac2021.ToString();
+            //}
+            //else if (arananyil == "2020")
+            //{
+            //    textBox3.Text = sayac2020.ToString();
+            //}
+            //else if (arananyil == "2019")
+            //{
+            //    textBox3.Text = sayac2019.ToString();
+            //}
+            //else if (arananyil == "2018")
+            //{
+            //    textBox3.Text = sayac2018.ToString();
+            //}
+            //else if (arananyil == "2017")
+            //{
+            //    textBox3.Text = sayac2017.ToString();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Var olmayan bir yıl girdiniz. Lütfen yılı baştan giriniz");
+            //}
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Title = "Excel Dosyaları";
+            save.DefaultExt = "txt";
+            save.Filter = "Text(*.txt)|.txt";
+            save.ShowDialog();
+            string dosya = save.FileName;
+
+            using (TextWriter tw = new StreamWriter(dosya)) //dosya+ ".txt";
+            {
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    tw.Write($"{dataGridView1.Columns[i].HeaderText.ToString()}");
+
+                    if (i != dataGridView1.Columns.Count - 1)
+                    {
+                        tw.Write(";");
+                    }
+
+                }
+                tw.WriteLine();
+
+                int satirsayisi = dataGridView1.RowCount;
+
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        tw.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
+
+                        if (j != dataGridView1.Columns.Count - 1)
+                        {
+                            tw.Write(";");
+                        }
+                    }
+                    tw.WriteLine();
+                }
+            }
+                text_kayit.Visible = true;
+                label_kayit.Visible = true;
+                text_kayit.Text = dataGridView1.Rows.Count.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            text_kayit.Visible = false;
+            label_kayit.Visible = false;
+        }
+
+    }
+}
